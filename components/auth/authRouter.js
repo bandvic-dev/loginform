e4fbee12dbfde50595f2aa286323e27c0540a358
Bandvic Dev <bandvic@mail.ru>
2023-12-24 11:50:44 +0200

    input rules add to config

--- components/auth/authRouter.js
+++ components/auth/authRouter.js
@@ -1,18 +1,12 @@
 import express from "express"
 import authController from "./authController.js"
 import roleMiddleware from "./middlewares/roleMiddleware.js"
-import { check } from "express-validator"
+import { config } from './config.js'
 const authRouter = express.Router()
 
-authRouter.post('/registration', [
-    check('username', 'User name can\'t be empty').notEmpty().isLength({min:3, max:15}),
-    check('password', 'Pasword must have min 5 max 8 symbols').isLength({min: 5, max: 8})
-], authController.registration)
+authRouter.post('/registration', config.rules.registration, authController.registration)
+authRouter.post('/login', config.rules.login, authController.login)
 
-authRouter.post('/login', [
-    check('username', 'User name can\'t be empty').notEmpty().isLength({min:3, max:15}),
-    check('password', 'Pasword must have min 5 max 8 symbols').isLength({min: 5, max: 8})
-], authController.login)
 authRouter.get('/logout', authController.logout)
 authRouter.get('/users', roleMiddleware(['ADMIN']), authController.getUsers)
 authRouter.get('/', authController.index)
--- components/auth/config.js
+++ components/auth/config.js
@@ -1,8 +1,19 @@
 import dotenv from 'dotenv'
+import { check } from "express-validator"
 
 const {AUTH_TOKEN_SECRET} = dotenv.config().parsed
 
 export const config = {
     secret: AUTH_TOKEN_SECRET,
-    expiresIn: '1h'
+    expiresIn: '1h',
+    rules: {
+        registration: [
+            check('username', 'User name can\'t be empty').isLength({min:3, max:15}).escape().trim(),
+            check('password', 'Pasword must have min 5 max 8 symbols').isLength({min: 5, max: 8}).escape().trim()
+        ],
+        login: [
+            check('username', 'User name can\'t be empty').isLength({min:3, max:15}).escape().trim(),
+            check('password', 'Pasword must have min 5 max 8 symbols').isLength({min: 5, max: 8}).escape().trim()
+        ],
+    }
 }
